cbe2e87445a5a60ed66377df8eabf850b22cbee9
igolubev <igolubevapp@gmail.com>
2021-02-10 14:54:58 +0300

    Create production webpack config and create container deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const packageJSON = require('../package.json');
+const commonConfig = require('./webpack.common');
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+      },
+      shared: packageJSON.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
