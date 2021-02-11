0cd8f9532ca89266508698069576facfb3e6fbf7
igolubev <igolubevapp@gmail.com>
2021-02-11 14:05:02 +0300

    Adding Loading Bar

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Header from './components/Header';
+import Progress from './components/Progress';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
@@ -17,7 +18,7 @@ export default () => {
       <StylesProvider generateClassName={ generateClassName }>
         <div>
           <Header />
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Progress />}>
             <Switch>
               <Route path="/auth" component={ AuthLazy } />
               <Route path="/" component={ MarketingLezy } />
